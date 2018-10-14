4216ef47d7e5363f694c82f7413bec7d99d266a1
Yi-Ren <yirenliu0909@gmail.com>
2018-10-15 01:46:21 +0800

    add script of run docker image

---
+++ cuda_env/run_cppt_env.sh
@@ -0,0 +1,30 @@
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker run \
+    --privileged \
+    -v /home/carl/datasets:/datasets \
+    -v /home/carl/catkin_ws:/catkin_ws \
+    -v /home/carl/docker_ws:/docker_ws \
+    -v /sys/kernel/debug:/sys/kernel/debug \
+    -it \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
+    cart0909/cppt_env:cuda \
+    bash
